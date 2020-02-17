1b91abd34bb3ace14a3dd2f253f98220295d9380
vegaharon <vegaharon@gmail.com>
2020-02-17 15:34:00 -0500

    reorganized, renamed and documented to code

--- src/scripts/createFunctions.sql
+++ src/scripts/createFunctions.sql
@@ -2,44 +2,53 @@
 CREATE OR REPLACE FUNCTION populate_gdelt() RETURNS void AS $$
 BEGIN
 
+--update person_id in the staging table
 UPDATE staging
 SET person_id = persons.id
 FROM persons
 WHERE staging.person = persons.person;
 
+--update theme_id in the staging table
 UPDATE staging
 SET theme_id = themes.id
 FROM themes
 WHERE staging.theme = themes.theme;
 
+--update organization_id in the staging table
 UPDATE staging
 SET organization_id = organizations.id
 FROM organizations
 WHERE staging.organization = organizations.organization;
 
+--insert persons that are new into persons table, they will acuire an id
 INSERT INTO persons(person)
 SELECT DISTINCT person FROM staging
 WHERE person_id is NULL;
 
+--insert organizations that are new into organizations table, they will acuire an id
 INSERT INTO organizations(organization)
 SELECT DISTINCT organization FROM staging
 WHERE organization_id is NULL;
 
+--update person_id in the staging table for the new persons
 UPDATE staging
 SET person_id = persons.id
 FROM persons
 WHERE staging.person = persons.person AND person_id IS NULL;
 
+--update organization_id in the staging table for the new organizations
 UPDATE staging
 SET organization_id = organizations.id
 FROM organizations
 WHERE staging.organization = organizations.organization AND organization_id IS NULL;
 
+--insert the the normalized columns into gdelt table 
 INSERT INTO gdelt 
 SELECT DISTINCT file_date, publication_id, 
 source_collection_id, person_id, theme_id, organization_id 
 FROM staging;
 
+--after loading a file the staging table empty the staging table 
 TRUNCATE TABLE staging;
 
 END; $$ 
@@ -49,6 +58,16 @@ LANGUAGE plpgsql;
 
 ###################
 
+CREATE OR REPLACE FUNCTION populate_elections() RETURNS void AS $$
+BEGIN
+
+--copy elections related records from elctions table to elections_production table and convert filedate to time stamp.  
+INSERT INTO elections_prod (file_date, person, theme, ave_tone, postivie_score, negative_score, polarity)
+SELECT to_timestamp(file_date::text, 'YYYYMMDDHH24MISS') as file_date, person, theme, ave_tone, postivie_score, negative_score, polarity 
+FROM elections WHERE (person LIKE '%bernie sanders%' OR person LIKE '%joe biden%' OR 
+person LIKE '%pete buttigieg%' OR person LIKE '%michael bloomberg%' OR 
+person LIKE '%amy klobuchar%' OR person LIKE '%elizabeth warren%' OR 
+person LIKE '%tom steyer%' OR person LIKE '%donald trump%') ;
 
 UPDATE elections_prod
 SET person = 'Bernie Sanders'
@@ -82,14 +101,13 @@ UPDATE elections_prod
 SET person = 'Elizabeth Warren'
 WHERE person like '%elizabeth warren%';
 
-DELETE FROM elections_prod WHERE person NOT IN ('Bernie Sanders', 'Joe Biden', 'Pete Buttigieg', 'Michael Bloomberg', 'Amy Klobuchar', 'Tom Steyer', 'Donald Trump');
+--delete irrelavant records just in case, there should not be any.
+DELETE FROM elections_prod WHERE person NOT IN ('Bernie Sanders', 'Joe Biden', 'Pete Buttigieg', 'Elizabeth Warren', 'Michael Bloomberg', 'Amy Klobuchar', 'Tom Steyer', 'Donald Trump');
 
-#query for tableau data source
+END; $$ 
+LANGUAGE plpgsql;
+
+--query for tableau as data source. 
 SELECT file_date, person, AVG(ave_tone) as tone, AVG(postivie_score) as postivie_score, AVG(negative_score) as negative_score, AVG(polarity) as polarity, count(*) as rec_count
 FROM elections_prod
 GROUP BY file_date, person;
-
-INSERT INTO elections_prod (file_date, person, theme, ave_tone, postivie_score, negative_score, polarity)
-SELECT to_timestamp(file_date::text, 'YYYYMMDDHH24MISS') as file_date, person, theme, ave_tone, postivie_score, negative_score, polarity 
-FROM elections WHERE person like '%elizabeth warren%';
-
--- src/scripts/getGdeltFiles.sh
+++ src/scripts/getGdeltFiles.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #This script downloads a gdelt file from the source, unzips it and saves in the S3 storage
 #Files are processed in a loop one at a time from the given start date to the end date.
+#Then it executes python spark job that ingests that file from S3 converts it to dataframe
+#selects some of the columns, renames them and archives them in S3 in parquet format
+#It also transforms it and saves in postgres DB
+#Then an SQL function is executed in postgres that does to further ths data processing to prepare it for 
+#further usage by a DS model or a front end dashboard. 
+#intermediae files get delited as they become unnesessary
 
 startDate="2020-01-31"
 endDate="2020-02-02"
@@ -21,16 +27,19 @@ while [ "$startDate" != "$endDate" ]; do
      start_time_loop=`date +%s`
      fileName=$(date -d "${startDate} + ${m} minutes" +"%Y%m%d%H%M%S")
          file=$gdeltPath$fileName$fileExt
+		 #download, unzip and store the input file in S3
          wget $file
          unzip $fileName$fileExt
          mv $fileName$fileExtUnziped /home/ubuntu/gitRoot/data/gdelt/$defaultFileName
          aws s3 mv /home/ubuntu/gitRoot/data/gdelt/currentGdeltFile.csv s3://gdeltstorage --exclude "*.sh"
-         rm $fileName$fileExt         
+         rm $fileName$fileExt
+         #execute spark code to transform the data 
          #spark-submit --master spark://ip-10-0-0-13:7077 --packages org.postgresql:postgresql:42.2.9 --jars /home/ubuntu/lib/postgresql-42.2.9.jar ~/gitRoot/Media-Sentiment-Explorer/src/python/Gdelt_gkg_file_ingester_elections.py
          spark-submit --packages org.postgresql:postgresql:42.2.9 --jars /home/ubuntu/lib/postgresql-42.2.9.jar Gdelt_gkg_file_ingester_elections.py
          current_time=`date +%s`
          runtime=$((current_time-start_time_loop))
          echo $runtime'  after spark submit' >> timing.log
+		 #exeucute an SQL function to further process the data
          ssh -i ~/.ssh/id_rsa ip-10-0-0-4.ec2.internal 'sudo -u postgres -i -H -- psql -c "SELECT populate_gdelt()"'
          current_time1=`date +%s`
          runtime=$((current_time1-current_time))
